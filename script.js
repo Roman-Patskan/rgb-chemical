2a25553d94070fa09222cea2b71bac301ccf5e46
annahnpu <annahnpu@gmail.com>
2022-05-16 18:06:53 +0300

    js by anna

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('test')
+let colors = ["00", "22", "44", "66", "77", "99", "ff"];
+colors.reverse()
+console.log(colors);
+
+let colorBoxRed = document.getElementById("color-red");
+let colorBoxGreen = document.getElementById("color-green");
+let colorBoxBlue = document.getElementById("color-blue");
+
+colors.forEach((item) => {
+    let colorElement = document.createElement("p");
+    colorElement.textContent = item;
+    colorElement.classList.add("control");
+    colorElement.dataset.index = "0";
+    colorBoxRed.insertAdjacentElement('beforeend', colorElement);
+    });
+
+
+colors.forEach((item) => {
+    let colorElement = document.createElement("p");
+    colorElement.textContent = item;
+    colorElement.classList.add("control");
+    colorElement.dataset.index = "1";
+    colorBoxGreen.insertAdjacentElement('beforeend', colorElement);
+    
+});
+
+colors.forEach((item) => {
+    let colorElement = document.createElement("p");
+    colorElement.textContent = item;
+    colorElement.classList.add("control");
+    colorElement.dataset.index = "2";
+    colorBoxBlue.insertAdjacentElement('beforeend', colorElement);
+   
+});
+
+
