f28ea62a19fc0027f36c74e174a10efdf8d15a0b
annahnpu <annahnpu@gmail.com>
2022-05-18 17:36:16 +0300

    Show Active Elements

--- script.js
+++ script.js
@@ -1,58 +1,35 @@
-let colors = ["00", "22", "44", "66", "77", "99",].reverse();
-let colorParts = ["3A", "C7", "50"];
+const colorParts = ["3A", "C7", "50"];
 const cloudPath = document.querySelector('#cloud-path');
 const textElement = document.querySelector('.text');
-colorChange();
+const colorBoxRed = document.getElementById("color-red");
+const colorBoxGreen = document.getElementById("color-green");
+const colorBoxBlue = document.getElementById("color-blue");
+const wrapperElement = document.getElementById("wrapper");
+const colorBoxes = [colorBoxRed, colorBoxGreen, colorBoxBlue];
+
 function colorChange() {
     let newColor = "#" + colorParts[0] + colorParts[1] + colorParts[2];
     cloudPath.style.fill = newColor;
     textElement.innerHTML = newColor;
 }
 
-let colorBoxRed = document.getElementById("color-red");
-let colorBoxGreen = document.getElementById("color-green");
-let colorBoxBlue = document.getElementById("color-blue");
-let wrapperElement = document.getElementById("wrapper");
-
-function createElement(colorBox, index, item) {
-    let colorElement = document.createElement("p");
-
-
-    activeColorElement = document.querySelector("active");
-
-    colorElement.textContent = item;
-    colorElement.classList.add("control");
-    colorElement.dataset.index = index;
-    colorBox.insertAdjacentElement('beforeend', colorElement);
-
-
-}
-
-
-
-colors.forEach((item) => {
-    createElement(colorBoxRed, 0, item);
-});
-
-
-colors.forEach((item) => {
-    createElement(colorBoxGreen, 1, item);
-});
-
-colors.forEach((item) => {
-    createElement(colorBoxBlue, 2, item);
-}); colors
+colorChange();
 
 wrapperElement.addEventListener('click', (event) => {
-    let currentClass = event.target.className;
-
+    const clickedElement = event.target;
+    const currentClass = clickedElement.className;
     if (currentClass == 'control') {
-
-        colorParts[event.target.dataset.index] = event.target.textContent;
-
+        removeActiveClasses(clickedElement.dataset.index);
+        clickedElement.classList.add("active");
+        colorParts[clickedElement.dataset.index] = clickedElement.textContent;
         colorChange();
     };
 });
 
 
-
+function removeActiveClasses(index) {
+    const elements = colorBoxes[index].querySelectorAll('.active')
+    elements.forEach((item) => {
+        item.classList.remove('active');
+    })
+}
